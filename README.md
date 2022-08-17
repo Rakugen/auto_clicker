28680d7cdf5c0db793c9a07b980872812b634682
Rakugen <simonlee0306@gmail.com>
2022-08-17 13:38:42 -0400

    Added some comments about functionality.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# PROJECT TITLE
+# Auto Clicker
 ## Description
-
+In an attempt to solve a problem I encountered in one of my current hobbies: how can I click faster without destroying my hands in cookie clicker?
+This python program uses threading and pynput module to create a toggle that will press mouse click repeatedly. 
 ## Installation
 
 ## How to use
@@ -9,3 +10,24 @@
 
 ## License
 
+MIT License
+
+Copyright (c) [2022] [Simon Lee]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- main.py
+++ main.py
@@ -1,55 +1,73 @@
 # This is a practice implementation for an auto-clicker that can be toggled start/stop/off and will click at high speeds
-# for the purpose of auto clicker games like Cookie Clicker.
+# for the purpose of auto clicker games like Cookie Clicker but can be expanded/modified to do other things that
+# involves mouse clicks and/or keyboard presses.
 
 import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 
-delay = 0.001
-button = Button.left
-start_stop_key = KeyCode(char="`")
-exit_key = KeyCode(char="=")
+# delay controls time between button presses
+DELAY = 0.001
+# button determines which mouse button to be pressed
+BUTTON = Button.left
+# start_stop and exit keys will control toggle for on/off and exit program
+START_STOP_KEY = KeyCode(char="`")
+EXIT_KEY = KeyCode(char="=")
 
 
+# Creating a new class that inherits from thread. This will allow us to overwrite threads run function with
+# our own as well as controlling when to start/stop/exit.
 class ClickMouse(threading.Thread):
+    # Inherit threads init() as well as adding our own arguments: delay/button from global.
+    # self.running and self.program_running will be used as control flow.
     def __init__(self, delay, button):
         super().__init__()
-        self.delay = delay
-        self.button = button
+        self.delay = DELAY
+        self.button = BUTTON
         self.running = False
         self.program_running = True
 
+    # Following 3 functions allow us to control our thread externally.
+    # This function will toggle on when our program starts the clicking functionality.
     def start_clicking(self):
         self.running = True
 
+    # This function will tell our program to stop clicking.
     def stop_clicking(self):
         self.running = False
 
+    # This tells our program to terminate.
     def exit(self):
         self.stop_clicking()
         self.program_running = False
 
+    # This function overwrote threads run() and will constantly execute whilst 'program_running' and 'running' is true.
+    # Inner loop is what the main program will be doing while it is running, in our case clicking 3 times and delay.
     def run(self):
         while self.program_running:
             while self.running:
                 mouse.click(self.button)
+                mouse.click(self.button)
+                mouse.click(self.button)
                 time.sleep(self.delay)
-            time.sleep(0.1)
+            # time.sleep(0.1)
+
 
 
 mouse = Controller()
-click_thread = ClickMouse(delay, button)
+# Instantiate our inheritted ClickMouse class
+click_thread = ClickMouse(DELAY, BUTTON)
 click_thread.start()
 
 
 def on_press(key):
-    if key == start_stop_key:
+    if key == START_STOP_KEY:
         if click_thread.running:
             click_thread.stop_clicking()
         else:
             click_thread.start_clicking()
-    elif key == exit_key:
+    elif key == EXIT_KEY:
         click_thread.exit()
         listener.stop()
 
